a290e43d867cff39745bd6705558f8378e58c97c
hoanglvuit <hoanglv2824@gmail.com>
2024-08-28 20:31:23 +0700

    first

--- code/generate_github_result.py
+++ code/generate_github_result.py
@@ -1,7 +1,7 @@
 from analyze import plot_certified_accuracy_per_sigma_best_model, Line, ApproximateAccuracy
 
-certification_result_with_denoiser = "data/certify/cifar10/mse_obj/MODEL_resnet110_90epochs_DENOISER_cifar10_dncnn_epochs_90/noise_0.25/test_N10000/sigma_0.25"
-certification_result_without_denoiser = "data/certify/cifar10/no_denoiser/MODEL_resnet110_90epochs/noise_0.00/test_N10000/sigma_0.25"
+certification_result_with_denoiser = "/kaggle/input/sigma0-25/sigma_0(1).25"
+certification_result_without_denoiser = "/kaggle/input/sigma0-25/sigma_0(1).25"
 
 plot_certified_accuracy_per_sigma_best_model(
     "./github_figures/readme_example", 'With vs Without Denoiser', 1.0,

172852b14b2e3da53b5a7ec9ff82e4bcf079fa28
hoanglvuit <hoanglv2824@gmail.com>
2024-08-28 20:36:59 +0700

    first

--- code/generate_github_result.py
+++ code/generate_github_result.py
@@ -1,7 +1,7 @@
 from analyze import plot_certified_accuracy_per_sigma_best_model, Line, ApproximateAccuracy
 
-certification_result_with_denoiser = "/kaggle/input/sigma0-25/sigma_0(1).25"
-certification_result_without_denoiser = "/kaggle/input/sigma0-25/sigma_0(1).25"
+certification_result_with_denoiser = "/kaggle/input/sigma0-25"
+certification_result_without_denoiser = "/kaggle/input/sigma0-25"
 
 plot_certified_accuracy_per_sigma_best_model(
     "./github_figures/readme_example", 'With vs Without Denoiser', 1.0,

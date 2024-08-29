76f714ff88af04a59929de890f691b9c189a1982
hoanglvuit <hoanglv2824@gmail.com>
2024-08-29 10:34:48 +0700

    fixxx

--- code/generate_github_result.py
+++ code/generate_github_result.py
@@ -4,7 +4,7 @@ certification_result_with_denoiser = "/kaggle/working/certification_output/sigma
 certification_result_without_denoiser = "/kaggle/working/certification_output/sigma_0.12"
 
 plot_certified_accuracy_per_sigma_best_model(
-    "/kaggle/working/", 'With vs Without Denoiser', 1.0,
+    "/kaggle/working/datasets", 'With vs Without Denoiser', 1.0,
     methods=
         [Line(ApproximateAccuracy(certification_result_with_denoiser), "$\sigma = 0.12$")],
     label='With Denoiser',

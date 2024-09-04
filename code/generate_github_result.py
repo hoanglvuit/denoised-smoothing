4a5a3f55f3446ff3f0e0625ecd3a4fa799d071f3
hoanglvuit <hoanglv2824@gmail.com>
2024-09-04 13:23:31 +0700

    fixxx

--- code/generate_github_result.py
+++ code/generate_github_result.py
@@ -4,7 +4,7 @@ certification_result_with_denoiser = "/kaggle/working/certification_output/sigma
 certification_result_without_denoiser = "/kaggle/working/certification_output/sigma_0.12"
 
 plot_certified_accuracy_per_sigma_best_model(
-    "/kaggle/working/", 'With vs Without Denoiser', 1.5,
+    "/kaggle/working/datasets", 'With vs Without Denoiser', 1.5,
     methods=
         [Line(ApproximateAccuracy(certification_result_with_denoiser), "$\sigma = 0.12$")],
     label='With Denoiser',
@@ -18,7 +18,7 @@ certification_result_with_denoiser = "/kaggle/working/certification_output/sigma
 certification_result_without_denoiser = "/kaggle/working/certification_output/sigma_0.25"
 
 plot_certified_accuracy_per_sigma_best_model(
-    "/kaggle/working/", 'With vs Without Denoiser', 1.5,
+    "/kaggle/working/datasets", 'With vs Without Denoiser', 1.5,
     methods=
         [Line(ApproximateAccuracy(certification_result_with_denoiser), "$\sigma = 0.25$")],
     label='With Denoiser',
@@ -33,7 +33,7 @@ certification_result_with_denoiser = "/kaggle/working/certification_output/sigma
 certification_result_without_denoiser = "/kaggle/working/certification_output/sigma_0.50"
 
 plot_certified_accuracy_per_sigma_best_model(
-    "/kaggle/working/", 'With vs Without Denoiser', 1.5,
+    "/kaggle/working/datasets", 'With vs Without Denoiser', 1.5,
     methods=
         [Line(ApproximateAccuracy(certification_result_with_denoiser), "$\sigma = 0.50$")],
     label='With Denoiser',
@@ -47,7 +47,7 @@ certification_result_with_denoiser = "/kaggle/working/certification_output/sigma
 certification_result_without_denoiser = "/kaggle/working/certification_output/sigma_1.00"
 
 plot_certified_accuracy_per_sigma_best_model(
-    "/kaggle/working/", 'With vs Without Denoiser', 1.5,
+    "/kaggle/working/datasets", 'With vs Without Denoiser', 1.5,
     methods=
         [Line(ApproximateAccuracy(certification_result_with_denoiser), "$\sigma = 1.00$")],
     label='With Denoiser',
